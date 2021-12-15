e19c91b9532c8eef85820ecf2b117fadf9c62e57
Joanne Silva <joanneegabriela@gmail.com>
2021-12-15 13:08:25 -0300

    Definiçao das variaveis de ambiente do front

---
+++ client/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  API_GRAPHQL_URL: 'http://localhost:3000/',
+  API_URL_HTTP: 'http://localhost:3000'
+};
---
+++ client/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  API_URL_GRAPHQL: 'http://localhost:3000/graphql',
+  API_URL_HTTP: 'http://localhost:3000'
+};
